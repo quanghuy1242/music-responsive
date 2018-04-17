396ba8c5cb57c29c50d78953d53bd32f892fd782
quanghuy1242 <katygaga124@gmail.com>
2018-04-17 22:10:26 +0700

    thêm trang btd.html

--- js.js
+++ js.js
@@ -11,5 +11,13 @@ $(function () {
             return $(title).children(".popover-heading").html();
         }
     });
-    $('[data-toggle="tooltip"]').tooltip(); 
+    $('[data-toggle="tooltip"]').tooltip();
+    document.getElementById("defaultplay").click();
+    document.getElementById("audioaudio").pause();
 });
+function abcd (){
+    var a = document.getElementById("player").offsetWidth;
+    var b = a - 73;
+    var c = b + 'px';
+    document.getElementById("audioaudio").style.width = c;
+}
